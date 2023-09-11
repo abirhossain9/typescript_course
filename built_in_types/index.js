b7eedb42c5d57e3d3cdc73ebb1a9c628f2266d81
abir16 <abir@shuttlebd.com>
2023-09-11 21:24:31 +0600

    Built in data types

---
+++ built_in_types/index.js
@@ -0,0 +1,11 @@
+var userId;
+var userName;
+var isActivated;
+var id = [1, 2, 3];
+userId = 10;
+userName = "Abir";
+isActivated = true;
+function displayInfo() {
+    console.log("Your name is ".concat(userName, ", id : ").concat(userId, ", Acticated status : ").concat(isActivated));
+}
+displayInfo();
---
+++ built_in_types/index.ts
@@ -0,0 +1,14 @@
+let userId : number
+let userName : string
+let isActivated : boolean
+let id = [1,2,3]
+
+userId = 10
+userName = "Abir"
+isActivated = true
+
+function displayInfo():void {
+    console.log(`Your name is ${userName}, id : ${userId}, Acticated status : ${isActivated}`)
+}
+
+displayInfo()
