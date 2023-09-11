c45b7a7b7bc62fc301ef53547d7d03cf7716f1ce
abir16 <abir@shuttlebd.com>
2023-09-11 20:57:28 +0600

    function call using typescript

---
+++ program_one/index.js
@@ -0,0 +1,5 @@
+var addNumbers = function (num1, num2) {
+    console.log(num1 + num2);
+};
+addNumbers(2, 5);
+addNumbers(2, 10);
---
+++ program_one/index.ts
@@ -0,0 +1,6 @@
+const addNumbers = (num1 : number , num2 : number) => {
+    console.log(num1+num2)
+}
+
+addNumbers(2,5)
+addNumbers(2,10)
