64736fec71b636dc84e2ca7a12edc331226396d0
abir16 <abir@shuttlebd.com>
2023-09-12 00:16:09 +0600

    array types

---
+++ user-defined-types/arrayTypes.js
@@ -0,0 +1,7 @@
+var userNames;
+userNames = ['abir', 'aaraz'];
+console.log(userNames[0]);
+var multipleTypes;
+multipleTypes = ['abir', 1];
+userNames.sort();
+console.log(userNames);
---
+++ user-defined-types/arrayTypes.ts
@@ -0,0 +1,11 @@
+let userNames : Array<string>
+
+userNames = ['abir', 'aaraz']
+
+console.log(userNames[0])
+
+let multipleTypes : (string|number)[]
+multipleTypes = ['abir', 1]
+userNames.sort()
+console.log(userNames)
+
