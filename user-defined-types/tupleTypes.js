ad6eb72f2af7d4a98592289ccd98d46a3dfcc612
abir16 <abir@shuttlebd.com>
2023-09-12 00:44:38 +0600

    tuple

---
+++ user-defined-types/tupleTypes.js
@@ -0,0 +1,4 @@
+var user;
+user = [23, "abir"];
+user.push(16, "raz");
+console.log(user[0]);
---
+++ user-defined-types/tupleTypes.ts
@@ -0,0 +1,4 @@
+let user : [number, string]
+user=[23,"abir"]
+user.push(16, "raz")
+console.log(user[0])
